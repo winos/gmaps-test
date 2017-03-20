40dca8dbdfbff9e4a68ff18253339abec1ecc1c5
winos <=>
2017-03-20 15:19:51 -0500

    added draw tool

--- js/app.js
+++ js/app.js
@@ -2,80 +2,55 @@ var map = undefined
 
 function initMap () {
 
+  var domMap = document.getElementById('map')
+  var latlng = new google.maps.LatLng(6.307955, -75.576715)
 
-    // buttons
-    var btnCalculate = document.getElementById('calculateBtn')
+  var myOptions = {
+    zoom: 19,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  }
 
-    var domMap = document.getElementById('map')
-    var latlng = new google.maps.LatLng(6.307955, -75.576715)
+  map = new google.maps.Map(domMap, myOptions)
 
-    var myOptions = {
-      zoom: 19,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.TERRAIN
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.RECTANGLE,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [
+        google.maps.drawing.OverlayType.CIRCLE,
+        google.maps.drawing.OverlayType.POLYGON,
+        google.maps.drawing.OverlayType.RECTANGLE
+      ]
+    },
+    //markerOptions: {icon: 'images/beachflag.png'},
+    circleOptions: {
+      fillColor: '#ffff00',
+      fillOpacity: 1,
+      strokeWeight: 5,
+      clickable: false,
+      editable: true,
+      zIndex: 1
     }
+  });
 
-    map = new google.maps.Map(domMap, myOptions)
+  drawingManager.setMap(map);
 
-    var checkPoint = createCheckPoint([
-      new google.maps.LatLng(6.307665, -75.576866),
-      new google.maps.LatLng(6.307688, -75.576536),
-      new google.maps.LatLng(6.307458, -75.576504),
-      new google.maps.LatLng(6.307455, -75.576835)
-    ])
+  google.maps.event.addListener(drawingManager, 'rectanglecomplete', function(polygon) {
+    //if (polygon.type == google.maps.drawing.OverlayType.RECTANGLE) {
+      //var coordinatesArray = polygon.overlay.getPaths().getArray();
+      console.log(polygon);
+    //}
+  });
 
-    map.addListener('click', function(e) {
+/*
+  google.maps.event.addListener(drawingManager, 'rectanglecomplete', function(polygon) {
+    drawingManager.setDrawingMode(null);
+    var arr=[];
+    polygon.getPath().forEach(function(latLng){arr.push(latLng.toString());})
+    alert(arr.join(',\n'));
+});*/
 
-      if (checkPoint(e.latLng)) {
-        var markerLatLng = {lat:e.latLng.lat(), lng:e.latLng.lng()}
-        ,   marker = new google.maps.Marker({
-              position: markerLatLng,
-              map: map,
-              title: 'Location'
-            })
-        map.setCenter(markerLatLng)
-      }
-    })
 
-    btnCalculate.addEventListener('click', function () {
-      // inputs
-      var origin = document.getElementById('origin').value
-      var destination = document.getElementById('destination').value
-
-      var mapRoute = new GoogleRoute({map: map})
-      mapRoute.calculateRoute(origin, destination)
-    }, false)
-}
-
-function GoogleRoute (options) {
-  this.options = options
-  this.map = this.options.map
-  this.directionService = new google.maps.DirectionsService()
-  this.directionsDisplay = new google.maps.DirectionsRenderer()
-  this.directionsDisplay.setMap(this.map)
-}
-
-GoogleRoute.prototype.calculateRoute = function (origin, destination) {
-
-  var request = {
-    origin: origin,
-    destination: destination,
-    provideRouteAlternatives: true,
-    //origin: new google.maps.LatLng(origin),
-    //destination: new google.maps.LatLng(destination),
-    travelMode: google.maps.TravelMode.DRIVING
-  }
-
-  var selfDirectionDisplay = this.directionsDisplay
-  this.directionService.route(request, function (result, status) {
-    if (status === google.maps.DirectionsStatus.OK)
-      selfDirectionDisplay.setDirections(result)
-  })
-}
-
-function createCheckPoint (points) {
-  this.checkPoint = new google.maps.Polygon({paths:points})
-  return function (eventLatLng) {
-    return google.maps.geometry.poly.containsLocation(eventLatLng, this.checkPoint)
-  }
 }
